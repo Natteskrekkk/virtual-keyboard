2c49a505d4da1007a5a4baddd03be177ed3f85bd
Timofey Gribanov <timofey.gribanov@gmail.com>
2023-05-03 19:41:44 +0300

    feat: add capsLock action

--- key.js
+++ key.js
@@ -1,9 +1,10 @@
 export default class Key {
-    constructor(symbol, style, actionProvider, keyEvent) {
+    constructor(symbol, style, actionProvider, keyEvent, capsSensetivity, shiftSensetivity) {
         this.symbol = symbol;
         this.style = style;
         this.actionProvider = actionProvider;
         this.keyEvent = keyEvent;
+        this.capsSensetivity = capsSensetivity;
     }
 }
 
